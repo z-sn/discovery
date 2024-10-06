d7f7bc8d6618552c02af9cba289ec0873e6ae54f
z-sn <z.sn.rust@gmail.com>
2024-10-06 14:55:32 -0700

    Done seiral and i2c

--- microbit/src/08-i2c/src/main.rs
+++ microbit/src/08-i2c/src/main.rs
@@ -1,10 +1,13 @@
-#![deny(unsafe_code)]
+//#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
 use cortex_m_rt::entry;
 use rtt_target::{rtt_init_print, rprintln};
 use panic_rtt_target as _;
+use core::fmt::Write;
+use heapless::Vec;
+use core::str::from_utf8;
 
 use microbit::hal::prelude::*;
 
@@ -16,8 +19,12 @@ use microbit::{
 
 #[cfg(feature = "v2")]
 use microbit::{
-    hal::twim,
+    hal::{twim, prelude::*, uarte, uarte::{Baudrate, Parity}},
     pac::twim0::frequency::FREQUENCY_A,
+};
+
+use lsm303agr::{
+    AccelOutputDataRate, MagOutputDataRate, Lsm303agr,
 };
 
 const ACCELEROMETER_ADDR: u8 = 0b0011001;
@@ -26,17 +33,65 @@ const MAGNETOMETER_ADDR: u8 = 0b0011110;
 const ACCELEROMETER_ID_REG: u8 = 0x0f;
 const MAGNETOMETER_ID_REG: u8 = 0x4f;
 
+#[cfg(feature = "v2")]
+mod serial_setup;
+#[cfg(feature = "v2")]
+use serial_setup::UartePort;
+
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
 
+    let mut serial = {
+        let serial = uarte::Uarte::new(
+            board.UARTE0,
+            board.uart.into(),
+            Parity::EXCLUDED,
+            Baudrate::BAUD115200,
+        );
+        UartePort::new(serial)
+    };
 
-    #[cfg(feature = "v1")]
-    let mut i2c = { twi::Twi::new(board.TWI0, board.i2c.into(), FREQUENCY_A::K100) };
-
-    #[cfg(feature = "v2")]
     let mut i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
+
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
+    sensor.init().unwrap();
+    sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
+    sensor.set_mag_odr(MagOutputDataRate::Hz50).unwrap();
+    
+    // Need this, otherwise gets stuck
+    let mut sensor = sensor.into_mag_continuous().ok().unwrap();
+
+    write!(serial, "Command : magnetometer or accelerometer \r\n");
+    let mut buffer: Vec<u8, 32> = Vec::new();
+    loop{
+        loop {
+            // Read from serial
+            let byte = nb::block!(serial.read()).unwrap();
+            buffer.push(byte);
+            if byte == 13 {
+                let command = from_utf8(&buffer).unwrap().trim();
+                if command == "magnetometer" {
+                    while !sensor.mag_status().unwrap().xyz_new_data{}
+                    let magdata = sensor.mag_data().unwrap();
+                    write!(serial, "Magnetometer:x {} y {} z {}\r\n", magdata.x, magdata.y, magdata.z).unwrap();
+                }
+                else if command == "accelerometer"
+                {
+                    while !sensor.accel_status().unwrap().xyz_new_data{}
+                    let data = sensor.accel_data().unwrap();
+                    write!(serial, "Acceleraton:x {} y {} z {}\r\n", data.x, data.y, data.z).unwrap();
+                }
+                else {
+                    write!(serial, "Invalid command: {}\r\n", command).unwrap();
+                }
+                buffer.clear();
+            }
+        }
+       
+    } 
+    /*
 
     let mut acc = [0];
     let mut mag = [0];
@@ -47,6 +102,6 @@ fn main() -> ! {
 
     rprintln!("The accelerometer chip's id is: {:#b}", acc[0]);
     rprintln!("The magnetometer chip's id is: {:#b}", mag[0]);
-
+*/
     loop {}
 }
